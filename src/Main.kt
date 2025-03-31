fd83514e483614a80199fb006e0b845d74888d73
DatNQ02 <datnq02@proxglobal.com>
2025-03-31 21:43:25 +0700

    add func encrypt/decrypt text

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,27 @@
 fun main() {
-    //128
-    val aes_128 = Aes(Aes.KeyLength.AES_128, "Key-test-cho-aes-128")
-    aes_128.encryptFile("src/input.txt", "src/out-enc-128.txt")
-    println(aes_128.getError())
-    aes_128.decryptFile("src/out-enc-128.txt", "src/out-dec-128.txt")
-    println(aes_128.getError())
+    val data = "Nhung that cay dang khi biet la"
+    val byteData = data.toByteArray()
+    println("rawData = $data")
+    print("byteArrayData = ")
+    byteData.printData()
 
-    //192
-    val aes_192 = Aes(Aes.KeyLength.AES_192, "Th-key-rat-nho")
-    aes_192.encryptFile("src/input.txt", "src/out-enc-192.txt")
-    println(aes_192.getError())
-    aes_192.decryptFile("src/out-enc-192.txt", "src/out-dec-192.txt")
-    println(aes_192.getError())
+    val aes128 = Aes(KeyLength.AES_128, "mot-cai-key-nao-do")
+    var encryptedData = byteArrayOf()
+    val encryptedTime = measureTime {
+        encryptedData = aes128.encryptByte(byteData)
+    }
+    print("encryptedData = ")
+    encryptedData.printData()
 
-    //256
-    val aes_256 = Aes(Aes.KeyLength.AES_256, "Th-key-rat-dai-pppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp")
-    aes_256.encryptFile("src/input.txt", "src/out-enc-256.txt")
-    println(aes_256.getError())
-    aes_256.decryptFile("src/out-enc-256.txt", "src/out-dec-256.txt")
-    println(aes_256.getError())
+    var decryptedData = byteArrayOf()
+    val decryptedTime = measureTime {
+        decryptedData = aes128.decryptByte(encryptedData)
+    }
+    print("decryptedData = ")
+    decryptedData.printData()
+
+    println("rawData = ${decryptedData.toString(Charsets.UTF_8)}")
+
+    println("encryptedTime = $encryptedTime us")
+    println("decryptedTime = $decryptedTime us")
 }
